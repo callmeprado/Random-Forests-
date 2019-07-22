a1b52adcecbd420b432b938eb623bccad4cfd7bc
callmeprado <52502374+callmeprado@users.noreply.github.com>
2019-07-22 16:11:41 -0400

    Add files via upload
    Classification

---
+++ RandomFC.py
@@ -0,0 +1,75 @@
+# Random Forest Classification  
+
+#importing Libraries 
+import numpy as np
+import matplotlib.pyplot as plt 
+import pandas as pd
+
+#importing Data set
+dataset = pd.read_csv('Social_Network_Ads.csv')
+
+X = dataset.iloc[:,[2,3]].values 
+Y = dataset.iloc[:,4].values
+
+#Splitting into Train & Test 
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size= 0.25, random_state = 0 )
+
+#Feature Scaling 
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+x_train = sc_X.fit_transform(x_train)
+x_test = sc_X.fit_transform(x_test)
+
+#Fitting RFC to training set 
+from sklearn.ensemble import RandomForestClassifier   
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+classifier.fit(x_train, y_train)
+
+#Predicting Test set results 
+y_pred = classifier.predict(x_test)
+
+#Making Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+count = 0
+for i in range( len(y_pred)):
+    if (y_pred[i]== 1): 
+        count = count + 1 
+        
+        
+#Vizualizing Training set results
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_train, y_train 
+X1, X2 = np.meshgrid(np.arange(start= x_set[:,0].min() - 1, stop = x_set[:,0].max() + 1, step = 0.01), 
+                     np.arange(start= x_set[:,1].min() - 1, stop = x_set[:,1].max() + 1, step = 0.01))
+plt.contour(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha = 0.75,
+            cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+for i,j in enumerate(np.unique(y_set )): 
+    plt.scatter(x_set[y_set == j,0], x_set[y_set == j,1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('RFC (Training Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+#Vizualizing Test set results
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_test, y_test
+X1, X2 = np.meshgrid(np.arange(start= x_set[:,0].min() - 1, stop = x_set[:,0].max() + 1, step = 0.01), 
+                     np.arange(start= x_set[:,1].min() - 1, stop = x_set[:,1].max() + 1, step = 0.01))
+plt.contour(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha = 0.75,
+            cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+for i,j in enumerate(np.unique(y_set )): 
+    plt.scatter(x_set[y_set == j,0], x_set[y_set == j,1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('RFC (Test Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
